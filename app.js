d84d332773989de49128f01b6d6ec5a09b732087
Engincan Varan <engincanvaran@gmail.com>
2021-03-28 20:02:21 +0300

    Middleware Examples & app.listen()

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     console.log("In the middleware 2");
     // ... send response etc...
+    res.send('<h1>Hello From Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
