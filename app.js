1ec0eded6f593e6c7cf59fe8345dd449b4b4902b
Engincan Varan <engincanvaran@gmail.com>
2021-03-28 20:26:37 +0300

    Adding Routes to expressJS

--- app.js
+++ app.js
@@ -5,29 +5,13 @@ const express = require('express');
 
 const app = express();
 
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    // console.log("This always runs!");
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-// only fire when the req is POST, GET, DELETE, PATCH
-app.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
-
-app.use('/', (req, res, next) => {
-    // console.log("In the middleware 2");
-    // ... send response etc...
-    res.send('<h1>Hello From Express!</h1>');
-});
+// DONT FORGET ORDER MATTER WHEN app.use()!!!
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(3000);
-// const server = http.createServer(app);
-// server.listen(3000);
