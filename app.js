a3cde2dd1cdf45fcb8a996a1962f930ffed0176a
Engincan Varan <engincanvaran@gmail.com>
2021-03-28 20:19:31 +0300

    Basic POST GET request and response & body-parser

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const http = require('http');
+const bodyParser = require("body-parser");
 
 const express = require('express');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/', (req, res, next) => {
-    console.log("This always runs!");
+    // console.log("This always runs!");
     next();
 });
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Add Product Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+// only fire when the req is POST, GET, DELETE, PATCH
+app.post("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
 });
 
 app.use('/', (req, res, next) => {
-    console.log("In the middleware 2");
+    // console.log("In the middleware 2");
     // ... send response etc...
     res.send('<h1>Hello From Express!</h1>');
 });
