1c40f5affa41dcc4bb342f8f1e5260fe4299ea63
Engincan Varan <engincanvaran@gmail.com>
2021-03-30 22:36:06 +0300

    Styling & Templating with EJS

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
-const path = require("path");
-
-const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.engine("hbs", expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: "hbs",
-}));
-app.set('view engine', 'hbs');
-// where are the views (htmls)
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require("./routes/admin");
-const shopRoute = require("./routes/shop");
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
-// static public folders such as css etc...
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// DONT FORGET ORDER MATTER WHEN app.use()!!!
-app.use('/admin', adminData.router);
-app.use(shopRoute);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    // res.status(404).send('<h1>Page not found</h1>');
-    // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-    res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,29 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("../util/path");
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
-// only fire when the req is POST, GET, DELETE, PATCH
-
-// /admin/add-product ==> GET
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: "Add Product",
-        path: '/admin/add-product',
-        activeProduct: true,
-        productCSS: true,
-        formCSS: true,
-    })
-    // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-// /admin/add-product ==> POST
-router.post("/add-product", (req, res, next) => {
-    products.push({title: req.body.title })
-    res.redirect("/");
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-exports.router = router;
+exports.routes = router;
 exports.products = products;
