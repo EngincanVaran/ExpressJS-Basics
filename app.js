f3a3571ce776934438de2d8181516e50ce37b71b
Engincan Varan <engincanvaran@gmail.com>
2021-03-30 21:59:57 +0300

    Styling & Templating with PUG

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const express = require('express');
 
 const app = express();
 
-const adminRoute = require("./routes/admin");
+app.set('view engine', 'pug');
+// where are the views (htmls)
+app.set('views', 'views');
+
+const adminData = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,12 +18,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // DONT FORGET ORDER MATTER WHEN app.use()!!!
-app.use('/admin', adminRoute);
+app.use('/admin', adminData.router);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
     // res.status(404).send('<h1>Page not found</h1>');
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render("404", {pageTitle: "Page Not Found"});
 });
 
 app.listen(3000);
